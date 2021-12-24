67fb88f619260f06cf76eeeabecdc9f3e610beb4
Sayed Sadat <SadatSayed92@gmail.com>
2021-12-24 00:26:20 -0800

    To Do List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const fakeTasks = ['Eat Dinner', 'Do Laundry', 'Go to Gym', 'Go Shopping'];
+
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTaskHandler = () => {
+    console.log('You Clicked AddTaskHandler Button');
+    console.log(task);
+    setTasks([task, ...tasks]);
+    console.log(tasks);
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2 style={{ color: 'white' }}>TODO LIST APP</h2>
+      <input type="text" style={{ outline: 'none' }} value={task} onChange={e => setTask(e.target.value)} />
+      <button onClick={addTaskHandler} >Add Task</button>
+
+      {tasks.map(task => (
+        <h2>{task}</h2>
+      ))}
+    </>
   );
 }
 
