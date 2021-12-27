f55097481e59b80ce95f6e29ae3db175924d9f79
Sayed Sadat <SadatSayed92@gmail.com>
2021-12-26 16:53:29 -0800

    Header

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
 import './App.css';
-import { useState } from 'react';
 import Header from './Components/Header';
 
 function App() {
-
-  const fakeTasks = ['Eat Dinner', 'Do Laundry', 'Go to Gym', 'Go Shopping'];
-
-  const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState('');
-
-  const addTaskHandler = () => {
-    console.log('You Clicked AddTaskHandler Button');
-    console.log(task);
-    setTasks([task, ...tasks]);
-    console.log(tasks);
-
-  }
-
-
-  return (
-    <>
-      <h2 style={{ color: 'white' }}>TODO LIST APP</h2>
-      <input type="text" style={{ outline: 'none' }} value={task} onChange={e => setTask(e.target.value)} />
-      <button onClick={addTaskHandler} >Add Task</button>
-
-      {tasks.map(task => (
-        <h2>{task}</h2>
-      ))}
-
-      <Header />
-    </>
-  );
+  return <Header />
 }
 
 export default App;
