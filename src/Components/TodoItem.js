7ea1138eb7cbb8470db18f57a077634ecd6a9d56
Sayed Sadat <SadatSayed92@gmail.com>
2021-12-29 23:17:29 -0800

    Edit Func to Input

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components';
 
 const TodoItem = ({ todo, todos, setTodos }) => {
+
+    const [editedTodo, setEditedTodo] = useState(todo.title);
+
+    useEffect(() => {
+        setEditedTodo(todo.title)
+    }, [todo])
 
     const deleteTask = () => {
         console.log(todo.id, todo.title);
         const currentTodoId = todo.id;
         setTodos(todos.filter(todo => todo.id != currentTodoId))
-        console.log(todos)
+        console.log(todos);
+    }
+
+    const saveTodo = () => {
+        const currentTodoId = todo.id
+        setTodos(todos.map(todo => todo.id === currentTodoId ? { ...todo, title: editedTodo } : todo))
     }
 
     return (
         <TodoListItem>
             <Checkbox className='far fa-circle' />
-            <input value={todo.title} />
-            <SaveTodo className='fas fa-check' />
+            <input value={editedTodo} onChange={e => setEditedTodo(e.target.value)} />
+            <SaveTodo className='fas fa-check' onClick={saveTodo} />
             <DeleteTodo className='fas fa-trash-alt' onClick={deleteTask} />
         </TodoListItem>
 
