d2b25822070335a38ee83d496dac09c5fbbd6436
Sayed Sadat <SadatSayed92@gmail.com>
2021-12-29 23:41:24 -0800

    Show Save Button When editing

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -33,7 +33,7 @@ const TodoItem = ({ todo, todos, setTodos }) => {
         <TodoListItem>
             <Checkbox className={todo.completed ? 'far fa-check-circle' : 'far fa-circle'} onClick={completeTodo} />
             <input style={{ textDecoration: todo.completed ? 'line-through' : 'none' }} value={editedTodo} onChange={e => setEditedTodo(e.target.value)} />
-            <SaveTodo className='fas fa-check' onClick={saveTodo} />
+            {todo.title != editedTodo && (<SaveTodo className='fas fa-check' onClick={saveTodo} />)}
             <DeleteTodo className='fas fa-trash-alt' onClick={deleteTask} />
         </TodoListItem>
 
